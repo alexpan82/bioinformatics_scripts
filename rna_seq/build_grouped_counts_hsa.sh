37a0168827e512def4c8698ff89a2bbbaa9cfa37
alexpan82 <apan82@gmail.com>
2021-10-13 08:07:08 -0400

    Remove redunant files

--- rna_seq/build_grouped_counts_hsa.sh
+++
@@ -1,8 +0,0 @@
-bash subread.sh  < <( echo hsa )
-
-ln -s counts.tsv counts.rep.tsv
-#cat counts.tsv | python ~osu8725/tools/toolbelt/scripts/replaceGencode.py | sed 's/-[0-9][0-9][0-9]//g' > counts.rep.tsv
-
-time python ~osu8725/tools/toolbelt/scripts/group_for_impute.py ../samples.txt 0 > clear.counts.tsv
-
-
